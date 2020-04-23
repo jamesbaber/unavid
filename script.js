e80c85a2b227f8deee4c78655a628535011a3326
james <admin@jamesbaber.co.uk>
2020-04-23 02:38:07 +0100

    Commenting js

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ function stringIsJSON(string) {
 }
 
 // Set the status line above the video
-function updateStatus() {
+function updateHeaderMessage() {
 	// Get current session token from page
 	oldSessionToken = document.getElementById("sessionToken").innerHTML;
 	// Only update if it's changed to stop the deselection issue
@@ -72,6 +72,7 @@ socket.onopen = function(e) {
 };
 
 socket.onmessage = function(event) {
+	// Check string is safe to parse before parsing into JSON format
 	if (stringIsJSON(event.data)) {
 		message = JSON.parse(event.data)
 
@@ -83,105 +84,143 @@ socket.onmessage = function(event) {
 		// Get handle on main player.
 		var mainPlayer = document.getElementById("mainPlayer");
 
-		// When media player pauses
+		// Media player pause event
 		mainPlayer.onpause = function(e) {
 			console.log("Pause event")
 
-			if (! surpressEventTransmission) {
+			// If event was generated locally by user
+			if (!surpressEventTransmission) {
+				// Transmit event to server
 				socket.send(JSON.stringify({
 					uuid: uuid,
 					command: "requestPauseMedia",
 					time: mainPlayer.currentTime,
 				}));
-				surpressEventTransmission = 0;
 			}
 		};
 
-		delayedPlay = 0
+		// Media player play event
 		mainPlayer.onplay = function(e) {
 			console.log("Play event")
-
+			// If event was generated locally by user
 			if (! surpressEventTransmission) {
+				// If the delay period before local media playback has elapsed
 				if (delayedPlay == 1) {
+					// Reset flag
 					delayedPlay = 0;
+					// Continue with event as usual
 					return;
 				}
+
+				// Try and stop the media player playing but probably won't work
+				e.preventDefault()
+				// Make sure the following pause isn't transmitted
+				surpressEventTransmission = 1;
+				// Pause temporarily
+				mainPlayer.pause()
+
+				// Wait 2x the connection latency before playing the video locally
+				var delay = (latency * 2);
+				setTimeout(function() {
+					delayedPlay = 1;
+					// If the socket is connected
+					if (socket.readyState === socket.OPEN) {
+						// Play locally
+						mainPlayer.play();
+						surpressEventTransmission = 0; // not sure about this
+					}
+				}, delay)
+
+				// Send event to server
 				socket.send(JSON.stringify({
 					uuid: uuid,
 					command: "requestPlayMedia",
 					time: mainPlayer.currentTime,
 				}));
-
-				e.preventDefault()
-				surpressEventTransmission = 1;
-				mainPlayer.pause()
-
-
-				var delay = (latency * 2);
-
-				setTimeout(function() {
-					delayedPlay = 1;
-					if (socket.readyState === socket.OPEN) {
-						mainPlayer.play();
-						surpressEventTransmission = 0;
-					}
-				}, delay)
 			}
 		};
 
+		// Media player seek event
 		mainPlayer.onseeked = function() {
 			console.log("Seek event")
-
-			if (! surpressEventTransmission) {
+			// If event was generated locally by user
+			if (!surpressEventTransmission) {
+				// Transmit event to server
 				socket.send(JSON.stringify({
 					uuid: uuid,
 					command: "requestSeek",
 					time: mainPlayer.currentTime,
 				}));
-				surpressEventTransmission = 0;
 			}
 		};
 
+		// If server sent an echo request
 		if (message.command == "echo") {
+			// Send echo back
 			socket.send(JSON.stringify({
 				command: "echoAck"
 			}))
-			latency = message.previousLatency,
-			sessionViewers = message.sessionViewers
-			updateStatus();
+
+			// Extract latency and viewer count from the server's original echo
+			latency = message.previousLatency;
+			sessionViewers = message.sessionViewers;
+
+			// Put new statistics on the page
+			updateHeaderMessage();
 		}
 
+		// First message from server. Set local UUID for all future communication to server
 		if (message.command == "setUUID") {
 			uuid = message.uuid;
+
+			// Reply with join session request and token
 			socket.send(JSON.stringify({
 				command: "joinSession",
 				token: sessionToken,
 			}));
 		}
 
+		// Set the media source - first main update from server. May need to be split out later
 		if (message.command == "setMediaSource") {
+			// Set session information
 			mainPlayer.src = message.sourceURL;
 			sessionToken = message.token
 
+			// Set initial media time
 			mainPlayer.currentTime = message.time;
+
+			// Set player state
 			if (message.state == 0) {
 				mainPlayer.pause()
 			} else if (message.state == 1) {
 				mainPlayer.play()
 			}
-			console.log(mainPlayer.readyState)
+			// Ensure the event doesn't get sent back out to the server - it already knows!
+			surpressEventTransmission = 1;
+
+			// Set the window URL get parameter to match the correct token as if the form was filled out correctly
 			window.history.replaceState({}, document.title, "/" + "player.html?t=" + sessionToken);
-			updateStatus();
+
+			// Update header message div
+			updateHeaderMessage();
 		}
 
+		// Pause media
 		if (message.command == "pauseMedia") {
+			// Ensure the pause event doesn't get sent back out to the server - it already knows!
 			surpressEventTransmission = 1;
+
+			// Set time and stop media playback
 			mainPlayer.currentTime = message.time;
 			mainPlayer.pause();
 		}
 
+		// Play media
 		if (message.command == "playMedia") {
+			// Ensure the play event doesn't get sent back out to the server - it already knows!
 			surpressEventTransmission = 1;
+
+			// Set time and start media playback
 			mainPlayer.currentTime = message.time;
 			mainPlayer.play();
 		}
